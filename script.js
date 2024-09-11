65c530aafb612ac3b926dc59e65e99fa9e62a243
MAHDIA001 <mahdianaiebi@gmail.com>
2024-09-11 21:44:08 +0500

    portfolio

---
+++ script.js
@@ -0,0 +1,13 @@
+function hide() {
+  const hide = document.querySelector(".sidebar");
+  hide.style.display = "none";
+}
+
+function show() {
+  const show = document.querySelector(".sidebar");
+  show.style.display = "flex";
+}
+
+const name = document.getElementById("name");
+const email = document.getElementById("email");
+const form = document.getElementById("form");
